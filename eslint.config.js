8febfc0affe4a8fe79d9aa563e0b97902101078d
younggsad <vel007evgeniy@gmail.com>
2026-07-21 18:46:47 +0300

    chore: setup monorepo structure with client and server workspaces

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,18 @@
 import js from "@eslint/js";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
 import globals from "globals";
 import tseslint from "typescript-eslint";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
 import eslintConfigPrettier from "eslint-config-prettier";
 
 export default tseslint.config(
-  { ignores: ["dist", "node_modules"] },
   {
+    ignores: ["**/dist", "**/node_modules"],
+  },
+
+  {
+    files: ["apps/client/**/*.{ts,tsx}"],
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -20,8 +23,23 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+      "react-refresh/only-export-components": [
+        "warn",
+        {
+          allowConstantExport: true,
+        },
+      ],
     },
   },
-  eslintConfigPrettier // всегда последним — гасит конфликтующие правила
+
+  {
+    files: ["apps/server/**/*.ts"],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    languageOptions: {
+      ecmaVersion: 2022,
+      globals: globals.node,
+    },
+  },
+
+  eslintConfigPrettier
 );
