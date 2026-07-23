9d14af5eca7b2bebe0cdc9d9ecf70a604a81eeca
younggsad <vel007evgeniy@gmail.com>
2026-07-23 19:41:26 +0300

    feat(server): setup Prisma with PostgreSQL

---
+++ apps/server/src/lib/prisma.ts
@@ -0,0 +1,12 @@
+import "dotenv/config";
+
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@/generated/prisma/client";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+export const prisma = new PrismaClient({
+  adapter,
+});
