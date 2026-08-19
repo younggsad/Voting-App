9539542aec2d4e7b177f66715fddc9854f34111b
younggsad <vel007evgeniy@gmail.com>
2026-08-19 21:45:47 +0300

    fix(client): exclude root layout from route tree

--- apps/client/src/routes/__root.tsx
+++ apps/client/src/routes/__root.tsx
@@ -1,6 +1,6 @@
 import { createRootRoute } from "@tanstack/react-router";
 
-import { RootLayout } from "./root-layout";
+import { RootLayout } from "./-root-layout";
 
 export const Route = createRootRoute({
   component: RootLayout,
